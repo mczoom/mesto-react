3b6357d15847316f057f4222dc0fb374d99a94fb
Andrew K <nscapital08@gmail.com>
2022-06-04 23:40:21 +0300

    feat: to make edit-profile form fully functional

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
+import EditProfilePopup from './EditProfilePopup';
 import api from '../utils/Api';
 import {CurrentUserContext} from '../contexts/CurrentUserContext';
 
@@ -26,7 +27,8 @@ function App() {
       })
       .catch(err => console.log(err));
 }, [])
-  
+
+ 
 
   function editProfile () {
     setEditProfilePopupState(!isEditProfilePopupOpen);
@@ -47,22 +49,24 @@ function App() {
     setSelectedCard(false);
   }
 
+  function handleUpdateUser (userInfo) {
+    api.setUserInfo(userInfo)
+        .then((data) => {
+          setCurrentUser(data);
+          closeAllPopups ();
+        })
+        .catch(err => console.log(err));
+  }
+  
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
     <div className="page">
       <Header />
       <Main onEditProfile={editProfile} onAddPlace={addPlace} onEditAvatar={editAvatar} onCardClick={setSelectedCard} />
-
-      <PopupWithForm title="Редактировать профиль" buttonText="Сохранить" name="edit-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-          <div className="popup__section">
-            <input className="popup__input popup__input_type_name" id="username-input" placeholder="Введите имя" name="username" type="text" minLength="2" maxLength="40" required />
-            <span className="popup__input-error username-input-error"></span>
-          </div>
-          <div className="popup__section">
-            <input className="popup__input popup__input_type_occupation" id="occupation-input" placeholder="Введите информацию о себе" name="useroccupation" type="text" minLength="2" maxLength="200" required />
-            <span className="popup__input-error occupation-input-error"></span>
-          </div>
-      </PopupWithForm>
+      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
+      
       <PopupWithForm title="Новое место" buttonText="Создать" name="add-item" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
           <div className="popup__section">
               <input className="popup__input popup__input_type_place" id="place-input" placeholder="Название" name="name" type="text" minLength="2" maxLength="30" required />
@@ -78,9 +82,8 @@ function App() {
               <input className="popup__input popup__input_type_url" id="link-input" placeholder="Ссылка на аватар" name="avatar" type="url" required />
               <span className="popup__input-error link-input-error"></span>
           </div>
-      </PopupWithForm>
-
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </PopupWithForm>     
+      
       <Footer />
   </div>
   </CurrentUserContext.Provider>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function PopupWithForm ({title, name, buttonText, isOpen, onClose, children}) {
+function PopupWithForm ({title, name, buttonText, isOpen, onClose, children, onSubmit}) {
     
     return(
       <div className={`popup popup-${name} ${isOpen ? 'popup_opened' : ''}`}>
         <div className="popup__container popup-edit-profile__container">
           <button className="popup__close-button" onClick={onClose}></button>
-          <form className="form popup__form" name={name} method="post" noValidate>          
+          <form className="form popup__form" onSubmit={onSubmit} name={name} method="post" noValidate>          
             <p className="popup__title">{title}</p>
             {children}
             <button className="popup__submit-button submit-button" type="submit">{buttonText}</button>
