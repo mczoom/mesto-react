51d61614a079d2bb983567aa7042fcdd0db4889a
Andrew K <nscapital08@gmail.com>
2022-06-05 00:26:53 +0300

    feat: to make edit-avatar form fully functional

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 import EditProfilePopup from './EditProfilePopup';
+import EditAvatarPopup from './EditAvatarPopup';
 import api from '../utils/Api';
 import {CurrentUserContext} from '../contexts/CurrentUserContext';
 
@@ -49,12 +50,21 @@ function App() {
     setSelectedCard(false);
   }
 
-  function handleUpdateUser (userInfo) {
+  function handleUpdateUser(userInfo) {
     api.setUserInfo(userInfo)
         .then((data) => {
           setCurrentUser(data);
           closeAllPopups ();
         })
+        .catch(err => console.log(err));
+  }
+
+  function handleUpdateAvatar(pic) {
+    api.setNewAvatar(pic)
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups ();
+      })
         .catch(err => console.log(err));
   }
   
@@ -66,6 +76,7 @@ function App() {
       <Main onEditProfile={editProfile} onAddPlace={addPlace} onEditAvatar={editAvatar} onCardClick={setSelectedCard} />
       <ImagePopup card={selectedCard} onClose={closeAllPopups} />
       <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
+      <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} onUpdateAvatar={handleUpdateAvatar}/>
       
       <PopupWithForm title="Новое место" buttonText="Создать" name="add-item" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
           <div className="popup__section">
@@ -76,13 +87,7 @@ function App() {
               <input className="popup__input popup__input_type_link" id="url-input" placeholder="Ссылка на картинку" name="link" type="url" required />
               <span className="popup__input-error url-input-error"></span>
           </div>
-      </PopupWithForm>
-      <PopupWithForm title="Обновить аватар" buttonText="Сохранить" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-          <div className="popup-edit-avatar__section">
-              <input className="popup__input popup__input_type_url" id="link-input" placeholder="Ссылка на аватар" name="avatar" type="url" required />
-              <span className="popup__input-error link-input-error"></span>
-          </div>
-      </PopupWithForm>     
+      </PopupWithForm>      
       
       <Footer />
   </div>
